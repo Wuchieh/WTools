1e8ddce3e8d9ff63abc6a132e768f4f395c68a4c
Wuchieh <q20001116@gmail.com>
2025-11-25 00:19:29 +0800

    fix: lint

--- app/components/ImagePreview.vue
+++ app/components/ImagePreview.vue
@@ -32,21 +32,21 @@
         </div>
 
         <div class="position-relative">
-            <div class="image-counter text-white bg-black rounded px-2 py-1 opacity-70 text-caption">
+            <div class="image-counter text-caption rounded bg-black px-2 py-1 text-white opacity-70">
                 {{ currentIndex + 1 }} / {{ store.images.length }}
             </div>
 
-            <swiper
-                :modules="[SwiperNavigation]"
-                navigation
-                loop
+            <Swiper
                 class="bg-grey-lighten-4 elevation-2 rounded-lg"
+                style="height: 400px;"
+                :modules="[SwiperNavigation]"
                 :slides-per-view="1"
                 :space-between="20"
-                style="height: 400px;"
+                loop
+                navigation
                 @slide-change="onSlideChange"
             >
-                <swiper-slide
+                <SwiperSlide
                     v-for="(img) in store.images"
                     :key="img.id"
                 >
@@ -58,25 +58,28 @@
                             :src="img.preview"
                             contain
                         >
-                            <div class="d-flex flex-column fill-height justify-space-between image-overlay">
-                                <div class="d-flex justify-end pa-4">
+                            <div class="d-flex fill-height flex-column image-overlay justify-space-between">
+                                <div class="d-flex pa-4 justify-end">
                                     <v-btn
-                                        color="error"
                                         class="mr-2"
+                                        color="error"
                                         icon="mdi-close"
                                         size="small"
                                         variant="flat"
                                         @click.stop="store.removeFile(img.id)"
                                     />
                                 </div>
-                                <div class="image-info pa-2 text-truncate rounded bg-black text-white opacity-70 mx-4 mb-4">
+                                <div
+                                    class="image-info pa-2 text-truncate mx-4 mb-4
+                                 rounded bg-black text-white opacity-70"
+                                >
                                     {{ img.file.name }} ({{ (img.file.size / 1024).toFixed(1) }} KB)
                                 </div>
                             </div>
                         </v-img>
                     </div>
-                </swiper-slide>
-            </swiper>
+                </SwiperSlide>
+            </Swiper>
         </div>
 
         <div class="d-flex mt-6 justify-center">
@@ -97,18 +100,23 @@
 </template>
 
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation as SwiperNavigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+
+import { Navigation as SwiperNavigation } from 'swiper/modules';
+import {
+    Swiper,
+    SwiperSlide,
+} from 'swiper/vue';
+
 import { useConverterStore } from '~/stores/converter';
 
 const store = useConverterStore();
 const currentIndex = ref(0);
 
-const onSlideChange = (swiper: any) => {
+function onSlideChange(swiper: any) {
     currentIndex.value = swiper.realIndex;
-};
+}
 </script>
 
 <style scoped>
